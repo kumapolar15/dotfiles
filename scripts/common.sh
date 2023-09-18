f086456f48dc346e4789b034a3e2a52b03f2dd43
kumapolar15 <kumapolar15.pg@gmail.com>
2023-09-18 16:21:09 +0900

    Modify: シェル実行時のカレントディレクトリを修正

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xeu
-export DOTFILES_REPO="$(cd "$(dirname "$0")" || exit 1; pwd)"
+export DOTFILES_REPO="$(cd "$(dirname "$0")/.." || exit 1; pwd)"
 
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -xeu
-export DOTFILES_REPO="$HOME/dotfiles"
+export DOTFILES_REPO="$DOTFILES_REPO:-$HOME/dotfiles"
 if [ ! -e "$DOTFILES_REPO" ]; then
   git clone https://github.com/kumapolar15/dotfiles.git "$DOTFILES_REPO"
 fi
 
 cd "$DOTFILES_REPO/scripts"
-source "$(dirname "$0")/install.sh"
-source "$(dirname "$0")/set-link.sh"
+"$(dirname "$0")/install.sh"
+"$(dirname "$0")/set-link.sh"
