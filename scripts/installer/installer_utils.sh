9361fe35dae457025fb8854b901b72d70d373b41
kumapolar15 <kumapolar15.pg@gmail.com>
2023-09-19 22:39:08 +0900

    Modify: shellcheckで指摘があった箇所を修正

--- scripts/installer/installer_utils.sh
+++ scripts/installer/installer_utils.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 get_os_bit() {
-  echo $(uname -m);
+  echo "$(uname -m)";
 }
 
 get_os_name() {
--- scripts/installer/zsh_installer.sh
+++ scripts/installer/zsh_installer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
+# shellcheck source=./installer_utils.sh
 source "$(dirname "$0")/installer_utils.sh"
 
 args=(zsh)
