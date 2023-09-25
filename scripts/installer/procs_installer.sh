4929ca848027da72cb585e21a1154a413ade63fa
kumapolar15 <kumapolar15.pg@gmail.com>
2023-09-25 18:19:28 +0900

    Modify: ubuntuのprocsインストール方法を修正

--- scripts/installer/procs_installer.sh
+++ scripts/installer/procs_installer.sh
@@ -5,5 +5,17 @@ source "$(dirname "$0")/../common.sh"
 source "$(dirname "$0")/installer_utils.sh"
 
 if [ "$(package_exist procs)" = 'Not exist!' ]; then
-  package_install procs
+  if [ "$(get_os_name)" = "Linux" ] && [ "$(get_linux_distribution)" = "ubuntu" ]; then
+    PROCS_VERSION=$(curl -s "https://api.github.com/repos/dalance/procs/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+    sudo wget -qO /usr/local/bin/procs.gz "https://github.com/dalance/procs/releases/latest/download/procs-v${PROCS_VERSION}-x86_64-lnx.zip"
+    sudo gunzip /usr/local/bin/procs.gz
+    sudo chmod a+x /usr/local/bin/procs
+  elif [ "$(get_os_name)" = "Linux" ] && [ "$(get_linux_distribution)" = "debian" ]; then
+    PROCS_VERSION=$(curl -s "https://api.github.com/repos/dalance/procs/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+    sudo wget -qO /usr/local/bin/procs.gz "https://github.com/dalance/procs/releases/latest/download/procs-v${PROCS_VERSION}-x86_64-lnx.zip"
+    sudo gunzip /usr/local/bin/procs.gz
+    sudo chmod a+x /usr/local/bin/procs
+  else
+    package_install procs
+  fi
 fi
