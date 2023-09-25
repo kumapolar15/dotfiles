e725cd58bd295d8a56049048e71b8318d6bbee68
kumapolar15 <kumapolar15.pg@gmail.com>
2023-09-25 18:31:00 +0900

    Modify: wgetをインストールするように修正

--- scripts/installer/procs_installer.sh
+++ scripts/installer/procs_installer.sh
@@ -6,11 +6,17 @@ source "$(dirname "$0")/installer_utils.sh"
 
 if [ "$(package_exist procs)" = 'Not exist!' ]; then
   if [ "$(get_os_name)" = "Linux" ] && [ "$(get_linux_distribution)" = "ubuntu" ]; then
+    if [ "$(package_exist wget)" = 'Not exist!' ]; then
+      package_install wget
+    fi
     PROCS_VERSION=$(curl -s "https://api.github.com/repos/dalance/procs/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
     sudo wget -qO /usr/local/bin/procs.gz "https://github.com/dalance/procs/releases/latest/download/procs-v${PROCS_VERSION}-x86_64-lnx.zip"
     sudo gunzip /usr/local/bin/procs.gz
     sudo chmod a+x /usr/local/bin/procs
   elif [ "$(get_os_name)" = "Linux" ] && [ "$(get_linux_distribution)" = "debian" ]; then
+    if [ "$(package_exist wget)" = 'Not exist!' ]; then
+      package_install wget
+    fi
     PROCS_VERSION=$(curl -s "https://api.github.com/repos/dalance/procs/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
     sudo wget -qO /usr/local/bin/procs.gz "https://github.com/dalance/procs/releases/latest/download/procs-v${PROCS_VERSION}-x86_64-lnx.zip"
     sudo gunzip /usr/local/bin/procs.gz
