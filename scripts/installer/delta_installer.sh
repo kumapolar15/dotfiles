5c882b3069e7c242605656bdab46b7835abb366e
kumapolar15 <kumapolar15.pg@gmail.com>
2023-09-25 19:03:20 +0900

    Modify: deltaのインストールスクリプトを修正

--- scripts/installer/delta_installer.sh
+++ scripts/installer/delta_installer.sh
@@ -5,7 +5,29 @@ source "$(dirname "$0")/../common.sh"
 source "$(dirname "$0")/installer_utils.sh"
 
 if [ "$(package_exist delta)" = 'Not exist!' ]; then
-  if [ "$(get_os_name)" = "Linux" ] && [ "$(get_linux_distribution)" = "alpine" ]; then
+  if [ "$(get_os_name)" = "Linux" ] && [ "$(get_linux_distribution)" = "ubuntu" ]; then
+    if [ "$(package_exist wget)" = 'Not exist!' ]; then
+      package_install wget
+    fi
+    if [ "$(package_exist tar)" = 'Not exist!' ]; then
+      package_install wget
+    fi
+    DELTA_VERSION=$(curl -s "https://api.github.com/repos/dandavision/delta/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+    sudo wget -qO /usr/local/bin/delta.tar.gz "https://github.com/dandavision/delta/releases/download/${DELTA_VERSION}/delta-${DELTA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+    sudo tar -zxvf /usr/local/bin/delta.tar.gz
+    sudo chmod a+x /usr/local/bin/delta
+  elif [ "$(get_os_name)" = "Linux" ] && [ "$(get_linux_distribution)" = "debian" ]; then
+    if [ "$(package_exist wget)" = 'Not exist!' ]; then
+      package_install wget
+    fi
+    if [ "$(package_exist tar)" = 'Not exist!' ]; then
+      package_install wget
+    fi
+    DELTA_VERSION=$(curl -s "https://api.github.com/repos/dandavision/delta/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+    sudo wget -qO /usr/local/bin/delta.tar.gz "https://github.com/dandavision/delta/releases/download/${DELTA_VERSION}/delta-${DELTA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+    sudo tar -zxvf /usr/local/bin/delta.tar.gz
+    sudo chmod a+x /usr/local/bin/delta
+  elif [ "$(get_os_name)" = "Linux" ] && [ "$(get_linux_distribution)" = "alpine" ]; then
     package_install delta
   else
     package_install git-delta
