c3485cdd287e4a081afdf92c57a2d54b2ed5f4af
kumapolar15 <kumapolar15.pg@gmail.com>
2023-09-25 18:28:40 +0900

    Modify: linterの指摘箇所を修正

--- scripts/installer/github_cli_installer.sh
+++ scripts/installer/github_cli_installer.sh
@@ -8,12 +8,12 @@ if [ "$(package_exist gh)" = 'Not exist!' ]; then
   if [ "$(get_os_name)" = "Linux" ] && [ "$(get_linux_distribution)" = "ubuntu" ]; then
     curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
     sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee -a /etc/apt/sources.list.d/github-cli.list > /dev/nul
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
     package_install gh
   elif [ "$(get_os_name)" = "Linux" ] && [ "$(get_linux_distribution)" = "debian" ]; then
     curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
     sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee -a /etc/apt/sources.list.d/github-cli.list > /dev/nul
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
     package_install gh
   elif [ "$(get_os_name)" = "Linux" ] && [ "$(get_linux_distribution)" = "oracle" ]; then
     package_install gh
