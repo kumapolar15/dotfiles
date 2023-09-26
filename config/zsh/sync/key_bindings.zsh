dc1d7a68c966633503482a771672bcd0ffe65ee6
kumapolar15 <kumapolar15.pg@gmail.com>
2023-09-26 15:18:50 +0900

    Feat: ghq widgetを設定

--- config/zsh/sync/key_bindings.zsh
+++ config/zsh/sync/key_bindings.zsh
@@ -1,11 +1,66 @@
 ### widgets ###
 widget::history() {
-    local selected="$(history -inr 1 | fzf --exit-0 --query "$LBUFFER" | cut -d' ' -f4- | sed 's/\\n/\n/g')"
-    if [ -n "$selected" ]; then
-        BUFFER="$selected"
-        CURSOR=$#BUFFER
+  local selected="$(history -inr 1 | fzf --exit-0 --query "$LBUFFER" | cut -d' ' -f4- | sed 's/\\n/\n/g')"
+  if [ -n "$selected" ]; then
+    BUFFER="$selected"
+    CURSOR=$#BUFFER
+  fi
+  zle -R -c # refresh screen
+}
+
+widget::ghq::source() {
+  local session color icon green="\e[32m" blue="\e[34m" reset="\e[m" checked="\uf631" unchecked="\uf630"
+  local sessions=($(tmux list-sessions -F "#S" 2>/dev/null))
+
+  ghq list | sort | while read -r repo; do
+    session="${repo//[:. ]/-}"
+    color="$blue"
+    icon="$unchecked"
+    if (( ${+sessions[(r)$session]} )); then
+      color="$green"
+      icon="$checked"
     fi
-    zle -R -c # refresh screen
+    printf "$color$icon %s$reset\n" "$repo"
+  done
+}
+
+widget::ghq::select() {
+  local root="$(ghq root)"
+  widget::ghq::source | fzf --exit-0 --preview="exa -lh ${(q)root}/{2}/{3}/{4} --color=always --icons" --preview-window="right:60%" | cut -d' ' -f2-
+}
+
+widget::ghq::dir() {
+  local selected="$(widget::ghq::select)"
+  if [ -z "$selected" ]; then
+    return
+  fi
+
+  local repo_dir="$(ghq list --exact --full-path "$selected")"
+  BUFFER="cd ${(q)repo_dir}"
+  zle accept-line
+  zle -R -c
+}
+
+widget::ghq::session() {
+  local selected="$(widget::ghq::select)"
+  if [ -z "$selected" ]; then
+    return
+  fi
+
+  local repo_dir="$(ghq list --exact --full-path "$selected")"
+  local session_name="${selected//[:. ]/-}"
+
+  if [ -z "$TMUX" ]; then
+    BUFFER="tmux new-session -A -s ${(q)session_name} -c ${(q)repo_dir}"
+    zle accept-line
+  elif [ "$(tmux display-message -p "#S")" = "$session_name" ] && [ "$PWD" != "$repo_dir" ]; then
+    BUFFER="cd ${(q)repo)_dir}"
+    zle accept-line
+  else
+    tmux new-session -d -s "$session_name" -c "$repo_dir" 2>/dev/null
+    tmux switch-client -t "$session_name"
+  fi
+  zle -R -c
 }
 
 function tree_select() {
@@ -46,11 +101,15 @@ function widget::filepath(){
 }
 
 zle -N widget::history
+zle -N widget::ghq::dir
+zle -N widget::ghq::session
 zle -N widget::filepath
 
 bindkey -v
-bindkey -M viins "^R"     widget::history     # C-r
-bindkey -M viins "^A"     beginning-of-line   # C-a
-bindkey -M viins "^E"     end-of-line         # C-e
-bindkey -M viins "^P"     widget::filepath    # C-p
-bindkey -M vicmd "/"      widget::history     # /
+bindkey "^R"              widget::history       # C-r
+bindkey "^G"              widget::ghq::session  # C-g
+bindkey "^[g"             widget::ghq::dir      # Alt-g
+bindkey -M viins "^A"     beginning-of-line     # C-a
+bindkey -M viins "^E"     end-of-line           # C-e
+bindkey -M viins "^P"     widget::filepath      # C-p
+bindkey -M vicmd "/"      widget::history       # /
