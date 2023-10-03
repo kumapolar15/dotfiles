1a28d37589f4f0855600bdf27fbd38b0e3ca948e
kumapolar15 <kumapolar15.pg@gmail.com>
2023-10-03 21:17:17 +0900

    Modify: lua_serverの設定

--- config/nvim/plugin/lspconfig.lua
+++ config/nvim/plugin/lspconfig.lua
@@ -61,3 +61,21 @@ mason_lsp.setup_handlers({
   end
 })
 
+nvim_lsp.lua_ls.setup({
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = { 'vim', 'require' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      telemetry = {
+        enable = false,
+      }
+    },
+  },
+})
