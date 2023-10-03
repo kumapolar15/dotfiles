afca39025bff2e12fcb408fda8f5b545906d5fa1
kumapolar15 <kumapolar15.pg@gmail.com>
2023-10-03 21:15:55 +0900

    Modify: lspsagaが正常に動作するように修正

--- config/nvim/after/plugin/lspsaga.rc.lua
+++ config/nvim/after/plugin/lspsaga.rc.lua
@@ -16,3 +16,21 @@ lspsaga.setup({
     imp_sign = '',
   }
 })
+
+local function show_documentation()
+  local ft = vim.opt.filetype._value
+  if ft == 'vim' or ft == 'help' then
+    vim.cmd([[execute 'h ' . expand('<cword>') ]])
+  else
+    vim.cmd([[Lspsaga hover_doc]])
+  end
+end
+
+vim.keymap.set('n', 'K', show_documentation)
+vim.keymap.set('n', 'ga', '<cmd>Lspsaga code_action<CR>')
+vim.keymap.set('n', 'grn', '<cmd>Lspsaga rename<CR>')
+vim.keymap.set('n', '[e', '<cmd>Lspsaga diagnostic_jump_prev<CR>')
+vim.keymap.set('n', ']e',  '<cmd>Lspsaga diagnostic_jump_prev<CR>')
+vim.keymap.set('n', 'grf', '<cmd>Lspsaga finder<CR>')
+vim.keymap.set('n', 'gd', '<cmd>Lspsaga peek_definition<CR>')
+vim.keymap.set('n', 'ge', '<cmd>Lspsaga show_line_diagnostics<CR>')
--- config/nvim/plugin/lspconfig.lua
+++ config/nvim/plugin/lspconfig.lua
@@ -1,30 +1,4 @@
-local status, nvim_lsp = pcall(require, 'lspconfig')
-
-local on_attach = function(client, bufnr)
-  local set = vim.keymap.set
-  local s, lspsaga = pcall(require, 'lspsaga')
-  if (not s) then
-    set('n', 'gr', '<cmd>Lspsaga finder<CR>')
-    set('n', 'gd', '<cmd>Lspsaga peek_definition<CR>')
-    set('n', 'K', '<cmd>Lspsaga hover_doc<CR>')
-    set('n', 'gn', '<cmd>Lspsaga rename<CR>')
-    set('n', 'ga', '<cmd>Lspsaga code_action<CR>')
-    set('n', 'ge', '<cmd>Lspsaga show_line_diagnostics<CR>')
-    set('n', '[e', '<cmd>Lspsaga diagnostic_jump_prev<CR>')
-    set('n', ']e', '<cmd>Lspsaga diagnostic_jump_next<CR>')
-  else
-    set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
-    set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
-    set('n', '<C-m>', '<cmd>lua vim.lsp.buf.signature_help()<CR>')
-    set('n', 'gy', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
-    set('n', 'rn', '<cmd>lua vim.lsp.rename()<CR>')
-    set('n', 'ma', '<cmd>lua vim.lsp.buf.code_action()<CR>')
-    set('n', 'gr', '<cmd>lua vim.lsp.buf.references<CR>')
-    set('n', '', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>')
-    set('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
-    set('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
-  end
-end
+local nvim_lsp = require('lspconfig')
 
 local protocol = require('vim.lsp.protocol')
 protocol.CompletionItemKind = {
@@ -55,11 +29,11 @@ protocol.CompletionItemKind = {
   '', -- TypeParameter
 }
 
-local status, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+local cmp_nvim_lsp = require('cmp_nvim_lsp')
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
-local status, mason = pcall(require, 'mason')
-local status, mason_lsp = pcall(require, 'mason-lspconfig')
+local mason = require('mason')
+local mason_lsp = require('mason-lspconfig')
 
 mason.setup({
   ui = {
@@ -82,7 +56,6 @@ mason_lsp.setup({
 mason_lsp.setup_handlers({
   function (server_name)
     nvim_lsp[server_name].setup({
-      on_attach = on_attach,
       capabilities = capabilities
     })
   end
