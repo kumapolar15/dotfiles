aa9d09563050981b5b4833b77ec460926158b76c
kumapolar15 <kumapolar15.pg@gmail.com>
2024-06-11 10:16:23 +0900

    Modify: MacOSの時のGithub CLIインストールパターンを追記

--- scripts/installer/github_cli_installer.sh
+++ scripts/installer/github_cli_installer.sh
@@ -5,7 +5,9 @@ source "$(dirname "$0")/../common.sh"
 source "$(dirname "$0")/installer_utils.sh"
 
 if [ "$(package_exist gh)" = 'Not exist!' ]; then
-  if [ "$(get_os_name)" = "Linux" ] && [ "$(get_linux_distribution)" = "ubuntu" ]; then
+  if [ "$(get_os_name)" = "MacOS" ]; then
+    package_install gh
+  elif [ "$(get_os_name)" = "Linux" ] && [ "$(get_linux_distribution)" = "ubuntu" ]; then
     curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
     sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
