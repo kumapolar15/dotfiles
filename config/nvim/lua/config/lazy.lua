ff161ba7229e1993dace3af2070770a100460103
kumapolar15 <kumapolar15.pg@gmail.com>
2024-08-01 11:30:50 +0900

    Modify: copilotプラグインのインストール

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -11,6 +11,8 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- import extras copilot
+    { import = "lazyvim.plugins.extras.coding.copilot" },
     -- import/override with your plugins
     { import = "plugins" },
   },
