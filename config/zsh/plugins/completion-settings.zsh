9c6ff326727d94b6c5607cc0c6452c3dccc33db6
kumapolar15 <kumapolar15.pg@gmail.com>
2026-02-03 15:09:15 +0900

    Feat: 補完関数の設定を追加

--- config/zsh/plugins/completion-settings.zsh
+++ config/zsh/plugins/completion-settings.zsh
@@ -1,3 +1,9 @@
+### 補完設定 ###
+# インストール直後のコマンドを即座に認識するためにrehash
+zstyle ':completion:*:commands' rehash 1
+# 大文字小文字を区別しない補完
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' '+m:{A-Z}={a-z}'
+# Cacheを用いた補完設定
 zstyle ':completion:*' use-cache yes
 _zcompcache="${XDG_CACHE_HOME}/zsh/zcompcache"
 if [ ! -d "${_zcompcache:h}" ]; then
