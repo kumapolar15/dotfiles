bfa9bbc09f25805bdc149d6a608e82ab7c00aa8b
kumapolar15 <kumapolar15.pg@gmail.com>
2026-02-03 15:07:26 +0900

    Fix: autoload compinitが複数回実行されている問題を修正

--- config/zsh/plugins/compinit.zsh
+++ config/zsh/plugins/compinit.zsh
@@ -1,10 +1,10 @@
 autoload -Uz compinit
 _zcompdump="${XDG_CACHE_HOME}/zsh/zcompdump-${HOST}-${ZSH_VERSION}"
-if [ ! -d "${_zcompdump:h}" ]; then
+if [[ ! -d "${_zcompdump:h}" ]]; then
     mkdir -p "${_zcompdump:h}"
 fi
-if [ -n "${_zcompdump}"(#qN.mh+24) ]; then
-    autoload -Uz compinit && compinit -d "${_zcompdump}"
+if [[ -n "${_zcompdump}"(#qN.mh+24) ]]; then
+    compinit -d "${_zcompdump}"
 else
-    autoload -Uz compinit && compinit -C -d "${_zcompdump}"
+    compinit -C -d "${_zcompdump}"
 fi
