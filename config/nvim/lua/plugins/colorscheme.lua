aa6658e8f3fd831ed56f357d1b970f228d609b98
kumapolar15 <129534535+kumapolar15@users.noreply.github.com>
2026-04-14 17:36:30 +0900

    Modify: nvimのcolorscheme変更時にハイライトを一括更新するautocmdの外枠を追加

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,12 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "iceberg",
+        callback = function()
+          -- Iceberg highlights for external plugins
+        end,
+      })
       vim.cmd([[colorscheme iceberg]])
     end,
   },
