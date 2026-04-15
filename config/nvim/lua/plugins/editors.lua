d0952b0286fb9e7c13c4ddeb641390eb867f42e9
kumapolar15 <129534535+kumapolar15@users.noreply.github.com>
2026-04-15 15:21:39 +0900

    Feat: nvimに囲い文字編集を追加

--- config/nvim/lua/plugins/editors.lua
+++ config/nvim/lua/plugins/editors.lua
@@ -3,4 +3,10 @@ return {
     "https://github.com/cohama/lexima.vim.git",
     event = "InsertEnter",
   },
+  {
+    "https://github.com/kylechui/nvim-surround.git",
+    version = "^4.0.0",
+    event = "VeryLazy",
+    opts = {},
+  },
 }
