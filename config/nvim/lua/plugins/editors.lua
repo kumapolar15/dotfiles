635d9f4e016b9778fa8976ee4ed5e10badc9b9a8
kumapolar15 <129534535+kumapolar15@users.noreply.github.com>
2026-04-15 16:50:56 +0900

    Feat: nvimにCommentingPluginを追加

--- config/nvim/lua/plugins/editors.lua
+++ config/nvim/lua/plugins/editors.lua
@@ -9,4 +9,25 @@ return {
     event = "VeryLazy",
     opts = {},
   },
+  {
+    "https://github.com/numToStr/Comment.nvim.git",
+    dependencies = {
+      {
+        "https://github.com/JoosepAlviste/nvim-ts-context-commentstring.git",
+        opts = {
+          enable_autocmd = false,
+        },
+      },
+    },
+    keys = {
+      { "gc", mode = { "n", "x" } },
+      { "gb", mode = { "n", "x" } },
+      { "gcc", mode = "n" },
+    },
+    config = function()
+      require("Comment").setup({
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      })
+    end,
+  },
 }
