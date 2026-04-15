a0e03b5bfa8f75ba6fbb4f689438b6c6346bdc40
kumapolar15 <129534535+kumapolar15@users.noreply.github.com>
2026-04-15 15:21:51 +0900

    Feat: nvimにwhich-keyを追加

---
+++ config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "https://github.com/folke/which-key.nvim.git",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  },
+}
