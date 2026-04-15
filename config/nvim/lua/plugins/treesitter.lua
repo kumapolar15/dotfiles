a7dc5b5ee636da00e8c4162cbb5d3e0a54ca8479
kumapolar15 <129534535+kumapolar15@users.noreply.github.com>
2026-04-15 16:23:01 +0900

    Feat: nvimにtreesitterを導入

---
+++ config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "https://github.com/nvim-treesitter/nvim-treesitter.git",
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
+    opts = {},
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("vim-treesitter-start", {}),
+        callback = function(ctx)
+          pcall(vim.treesitter.start)
+        end,
+      })
+    end
+  },
+}
