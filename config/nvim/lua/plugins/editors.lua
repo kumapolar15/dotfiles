ad2ca4056c23ef5fef3a2f74246632b32e79c9f8
kumapolar15 <129534535+kumapolar15@users.noreply.github.com>
2026-04-16 18:23:10 +0900

    Feat: nvimの値加算減算を強化する機能を追加

--- config/nvim/lua/plugins/editors.lua
+++ config/nvim/lua/plugins/editors.lua
@@ -30,4 +30,50 @@ return {
       })
     end,
   },
+  {
+    "https://github.com/monaqa/dial.nvim.git",
+    lazy = true,
+    keys = {
+      {
+        "<C-a>", function()
+          require("dial.map").manipulate("increment", "normal")
+        end, mode = "n",
+      },
+      {
+        "<C-x>", function()
+          require("dial.map").manipulate("decrement", "normal")
+        end, mode = "n",
+      },
+      {
+        "g<C-a>", function()
+          require("dial.map").manipulate("increment", "gnormal")
+        end, mode = "n",
+      },
+      {
+        "g<C-x>", function()
+          require("dial.map").manipulate("decrement", "gnormal")
+        end, mode = "n",
+      },
+      {
+        "<C-a>", function()
+          require("dial.map").manipulate("increment", "visual")
+        end, mode = "x",
+      },
+      {
+        "<C-x>", function()
+          require("dial.map").manipulate("decrement", "visual")
+        end, mode = "x",
+      },
+      {
+        "g<C-a>", function()
+          require("dial.map").manipulate("increment", "gvisual")
+        end, mode = "x",
+      },
+      {
+        "g<C-x>", function()
+          require("dial.map").manipulate("decrement", "gvisual")
+        end, mode = "x",
+      },
+    },
+  },
 }
