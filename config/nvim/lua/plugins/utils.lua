be55ee3971cb513ccae6fc1954cd5b16dc4faf25
kumapolar15 <129534535+kumapolar15@users.noreply.github.com>
2026-04-23 11:34:30 +0900

    Modify: nvimのhttpリクエスト送信時にssl_verificationをスキップするように変更

--- config/nvim/lua/plugins/utils.lua
+++ config/nvim/lua/plugins/utils.lua
@@ -81,6 +81,13 @@ return {
     },
     ft = "http",
     build = false,
+    config = function ()
+      vim.g.rest_nvim = {
+        request = {
+          skip_ssl_verification = true,
+        },
+      }
+    end
   },
 }
 
