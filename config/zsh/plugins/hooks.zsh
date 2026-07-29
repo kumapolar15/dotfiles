c2181cb5fade6c89df6e1dcd8f61be3ce7d325e7
kumapolar15 <129534535+kumapolar15@users.noreply.github.com>
2026-07-29 18:18:59 +0900

    Fix: ファイル数が膨大なディレクトリに移動した際に硬直する問題を修正
    ディレクトリ移動時に発火するhook関数を、ファイル数が多い場合は無効化するように変更

--- config/zsh/plugins/hooks.zsh
+++ config/zsh/plugins/hooks.zsh
@@ -2,10 +2,32 @@ zshaddhistory(){
   local line="${1%%$'\n'}"
   [[ ! "$line" =~ "^(cd|jj?|lazygit|la|ll|ls|rm|rmdir)($| )" ]]
 }
-chpwd(){
-  if [[ $(pwd) != "$HOME" ]]; then
-    ls -l
+
+__chpwd_ll(){
+  local -r display_rows="$1"
+  if (( $+commands[eza] )); then
+    eza -l --icons=always --color=always | head -n "$display_rows"
   else
+    ls -l | head -n "$display_rows"
+  fi
+}
+chpwd(){
+  local -r LIMIT_DISABLE_SORT_COUNT=1000
+  local -r DISPLAY_ROWS=20
+
+  if [[ $(pwd) = "$HOME" ]]; then
     echo "Welcome home!"
+  else
+    line_count=$(\find . | head -n $((LIMIT_DISABLE_SORT_COUNT + 1)) | wc -l)
+
+    if [[ "$line_count" -le "$DISPLAY_ROWS" ]];then
+      __chpwd_ll $DISPLAY_ROWS
+    elif [[ "$line_count" -le "$LIMIT_DISABLE_SORT_COUNT" ]];then
+      local -r remaining=$((line_count - DISPLAY_ROWS))
+      __chpwd_ll $DISPLAY_ROWS
+      echo "\n... and $remaining more items (total $line_count)"
+    else
+      echo -e "[\e[33mWARN\e[0m]: Over $LIMIT_DISABLE_SORT_COUNT files/directories detected! 'chpwd' has been disabled."
+    fi
   fi
 }
